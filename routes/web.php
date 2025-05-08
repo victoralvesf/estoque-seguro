d9cf5372e81c82dbd4733a4b4d491316ca95ed29
Victor Alves <viictor.alvess12@gmail.com>
2025-05-08 10:30:49 -0300

    feat: redirect users to login when accessing the home page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('login');
+    return redirect()->route('login');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
