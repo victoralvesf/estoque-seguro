789c9b74d597a246b480050f5802273ee9fe0e52
Victor Alves <viictor.alvess12@gmail.com>
2025-05-09 10:25:18 -0300

    feat: add role to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use InvalidArgumentException;
 
 class User extends Authenticatable
 {
@@ -21,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +47,38 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function role(): Attribute
+    {
+        return Attribute::make(
+            set: function (string $value) {
+                $validRoles = ['admin', 'operator', 'user'];
+                $validRolesText = implode(",", $validRoles);
+
+                if (!in_array($value, $validRoles)) {
+                    throw new InvalidArgumentException(
+                        "Role '{$value}' is not valid. Valid roles are: {$validRolesText}."
+                    );
+                }
+
+                return $value;
+            }
+        );
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isOperator(): bool
+    {
+        return $this->role === 'operator';
+    }
+
+    public function isRegularUser(): bool
+    {
+        return $this->role === 'user';
     }
 }
