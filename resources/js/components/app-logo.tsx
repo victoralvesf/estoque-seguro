c6c53f91d424717bc9d64ef8e97ae65970217856
Victor Alves <viictor.alvess12@gmail.com>
2025-05-09 09:26:08 -0300

    refactor: use default app name from shared data on app logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,17 @@
+import { usePage } from '@inertiajs/react';
 import AppLogoIcon from './app-logo-icon';
+import { SharedData } from '@/types';
 
 export default function AppLogo() {
+    const { name } = usePage<SharedData>().props
+
     return (
         <>
             <div className="bg-emerald-950 border border-emerald-900 flex aspect-square size-8 items-center justify-center rounded-md">
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="truncate font-semibold">Estoque Seguro</span>
+                <span className="truncate font-semibold">{name}</span>
             </div>
         </>
     );
