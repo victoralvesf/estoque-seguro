3027e5f1f88b9db312e5eeda876b60199f82ca6c
Victor Alves <viictor.alvess12@gmail.com>
2025-05-09 12:47:19 -0300

    feat: add products migration

---
+++ database/migrations/2025_05_09_150945_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->string('currency_code')->default('BRL');
+            $table->string('category');
+            $table->string('sku')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
