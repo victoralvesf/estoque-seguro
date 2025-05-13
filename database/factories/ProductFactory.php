b4112509f73463b387ebc2122c17e01cccfd3f50
Victor Alves <viictor.alvess12@gmail.com>
2025-05-13 03:27:05 -0300

    chore: update product and category factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +11,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ProductFactory extends Factory
 {
+    protected $model = Product::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,12 +21,12 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->sentence(),
+            'name' => fake()->words(3, true),
             'description' => fake()->paragraph(),
-            'quantity' => fake()->randomNumber(2),
-            'price' => fake()->randomFloat(2, 1, 999999.99),
+            'quantity' => fake()->randomNumber(3),
+            'price' => fake()->randomFloat(2, 10, 29999.99),
             'currency_code' => 'BRL',
-            'category' => fake()->randomElement(['Eletrônicos', 'Roupas', 'Alimentos', 'Móveis', 'Brinquedos', 'Ferramentas']),
+            'category_id' => Category::factory(),
             'sku' => fake()->unique()->ean13(),
         ];
     }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -12,6 +12,6 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory(100)->create();
+        Product::factory(50)->create();
     }
 }
